b35833ee739f3263863d0e4e621c8bb75345956c
sankargk7 <159630058+sankargk7@users.noreply.github.com>
2024-08-27 20:34:01 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,34 @@
+# resource "google_storage_bucket" "static" {
+#  name          = "python-practice-407605-new123456"
+#  location      = "US"
+#  storage_class = "STANDARD"
+#  uniform_bucket_level_access = true
+# }
+
+resource "google_compute_instance" "vm" {
+  name              = "terraform-instance-1234"
+  machine_type      = "n2-standard-2"
+  zone              = "us-central1-a"
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+}
+
+provider "google" {
+    project = "data-air-433813-q7"
+    credentials = "terraformcloudbuild.json"
+
+}
