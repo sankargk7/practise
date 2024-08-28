09a0dce2f7b980c6e51cc24cdea101cf5c1d0799
sankargk7 <sankar.gk.7@gmail.com>
2024-08-28 10:48:19 +0530

    pubsub

--- main.tf
+++ main.tf
@@ -45,11 +45,14 @@ resource "google_storage_notification" "notification" {
 
 // Enable notifications by giving the correct IAM permission to the unique service account.
 
+data "google_storage_project_service_account" "gcs_account" {
+}
+
 
 resource "google_pubsub_topic_iam_binding" "binding" {
   topic   = google_pubsub_topic.topic.id
   role    = "roles/pubsub.publisher"
-  members = ["serviceAccount:test-sa@data-air-433813-q7.iam.gserviceaccount.com"]
+  members = ["serviceAccount:${data.google_storage_project_service_account.gcs_account.email_address}"]
 }
 
 // End enabling notifications
