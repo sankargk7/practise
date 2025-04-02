0abcd45217e57425d40cb84e97aa4abd180fe2a1
sankargk7 <sankar.gk.7@gmail.com>
2025-04-02 12:18:13 +0530

    Service account creation

--- main.tf
+++ main.tf
@@ -105,21 +105,28 @@ provider "google" {
 #   role   = "roles/cloudfunctions.invoker"
 #   member = "allUsers"
 # }
+#################################
 
-# Create a BigQuery dataset for billing export
-resource "google_bigquery_dataset" "billing_export" {
-  dataset_id = "billing_data"
-  project    = "evident-display-455412-k1"
-  location   = "US"
-
-  labels = {
-    environment = "billing"
-  }
+resource "google_service_account" "billing_sa" {
+  account_id   = "billing-sa"
+  display_name = "Billing Service Account"
+  description  = "This is a service account created via Terraform"
 }
 
-# Grant permissions to the Billing Export system service account
-resource "google_bigquery_dataset_iam_member" "billing_export_writer" {
-  dataset_id = google_bigquery_dataset.billing_export.dataset_id
-  role       = "roles/bigquery.dataEditor"
-  member     = "serviceAccount:billing-export-system@system.gserviceaccount.com"
-}
+# # Create a BigQuery dataset for billing export
+# resource "google_bigquery_dataset" "billing_export" {
+#   dataset_id = "billing_data"
+#   project    = "evident-display-455412-k1"
+#   location   = "US"
+
+#   labels = {
+#     environment = "billing"
+#   }
+# }
+
+# # Grant permissions to the Billing Export system service account
+# resource "google_bigquery_dataset_iam_member" "billing_export_writer" {
+#   dataset_id = google_bigquery_dataset.billing_export.dataset_id
+#   role       = "roles/bigquery.dataEditor"
+#   member     = "serviceAccount:billing-export-system@system.gserviceaccount.com"
+# }
