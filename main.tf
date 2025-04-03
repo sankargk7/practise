c6416a9799fa41d0e062e90023926731bcd4bd55
sankargk7 <sankar.gk.7@gmail.com>
2025-04-03 09:08:09 +0530

    VM

--- main.tf
+++ main.tf
@@ -130,3 +130,53 @@ resource "google_bigquery_dataset_iam_member" "billing_export_writer" {
   role       = "roles/bigquery.dataEditor"
   member     = "serviceAccount:billing-sa@evident-display-455412-k1.iam.gserviceaccount.com"
 }
+
+
+##############################
+
+resource "google_service_account" "default" {
+  account_id   = "my-custom-sa"
+  display_name = "Custom SA for VM Instance"
+}
+
+resource "google_compute_instance" "default" {
+  name         = "my-instance"
+  machine_type = "n2-standard-2"
+  zone         = "us-central1-a"
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "value"
+      }
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "NVME"
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata = {
+    foo = "bar"
+  }
+
+  metadata_startup_script = "echo hi > /test.txt"
+
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    email  = google_service_account.default.email
+    scopes = ["cloud-platform"]
+  }
+}
